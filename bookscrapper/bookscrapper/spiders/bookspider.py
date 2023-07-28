45e37eb929fe82d5bdede6f2a9ffee7e78112ac0
Alex Njuguna <123743552+alex-njuguna@users.noreply.github.com>
2023-07-28 18:09:26 +0300

    Update bookspider.py

--- bookscrapper/bookscrapper/spiders/bookspider.py
+++ bookscrapper/bookscrapper/spiders/bookspider.py
@@ -7,4 +7,20 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        books = response.css("article.product_pod")
+        
+        for book in books:
+            yield{
+                'name': book.css("h3 a::text").get(),
+                'price': book.css(".product_price .price_color::text").get(),
+                'url': book.css("h3 a").attrib["href"]
+            }
+        next_page = response.css("li.next a::attr(href)").get()
+        
+        if next_page is not None:
+            if "catalogue/" in next_page:
+                next_page_url = "https://books.toscrape.com" + next_page
+            else:
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
+        
+        yield response.follow(next_page_url, callback=self.parse)
